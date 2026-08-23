b42c368fd9c4658c4d450cdf27d47f8969f40d2e
Steven Rey Sequeira <stevdrey@gmail.com>
2026-08-23 16:14:21 -0600

    feat: add backend application entry point

---
+++ backend/src/main/java/io/github/stevdrey/dokene/DokeneApplication.java
@@ -0,0 +1,12 @@
+package io.github.stevdrey.dokene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DokeneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DokeneApplication.class, args);
+    }
+}
